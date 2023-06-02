4f819075f29ece0dff026fdac268e0e2ee12c29c
thomas-rooty <tc.caron91@gmail.com>
2023-06-02 21:29:22 +0200

    API Const

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,20 +1,22 @@
+const api = 'http://192.168.1.61:5000/api'
+
 // Get all characters from API
 export const getAllCharacters = async () => {
-  const uri = 'http://10.74.1.120:5000/api/characters'
+  const uri = `${api  }/characters`
   const response = await fetch(uri)
   return response.json()
 }
 
 // Get all the scenarios from API
 export const getAllScenarios = async () => {
-  const uri = 'http://10.74.1.120:5000/api/scenarios'
+  const uri = `${api  }/scenarios`
   const response = await fetch(uri)
   return response.json()
 }
 
 // Get all the unplanned from API
 export const getAllUnplanned = async () => {
-  const uri = 'http://10.74.1.120:5000/api/unplanned'
+  const uri = `${api  }/unplanned`
   const response = await fetch(uri)
   return response.json()
 }
